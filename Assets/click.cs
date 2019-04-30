b9452e2396b55b04e166d2bbc7ba8f1ff1b33b5b
Alan Ho <Alan@Alans-MacBook-Pro-2.local>
2019-04-30 15:35:38 -0400

    first commit

---
+++ Assets/click.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement; // <<<<<< ADD THIS.
+
+public class click : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Room");
+    }
+}
---
+++ Assets/move_char.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_char : MonoBehaviour
+{
+
+    public void move() {
+        transform.position = GameObject.Find("Cube").transform.position;
+
+    }
+}
